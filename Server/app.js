6dd976185c72c8d6886fc8395b758d1cac12bb3f
davidkenesaw <77091094+davidkenesaw@users.noreply.github.com>
2023-08-29 15:33:19 -0400

    logging host

--- Server/app.js
+++ Server/app.js
@@ -19,4 +19,5 @@ app.get('/',(req,res)=>{
 
 app.listen(port, ()=>{//server listens on port 3000
     console.log("website hosted on port " + port)
+    console.log("go to localhost:3000 in your Browser")
 })
